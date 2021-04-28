d72b1fa7b460d14fe55ee09f580c174d709952e7
mario-15ra <mario.ra@students.salle.url.edu>
2021-04-28 19:19:42 +0200

    Settings hecho y borra cuenta cuando sepamos quien es el que esta jugando.

--- BetweenUs/src/controller/SettingViewController.java
+++ BetweenUs/src/controller/SettingViewController.java
@@ -2,18 +2,21 @@ package controller;
 
 import model.User;
 import model.UserManager;
+import model.database.SQLoperations;
 import view.LoginView;
 import view.RegisterView;
 import view.SettingView;
 
+import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-import static model.database.SQLoperations.registreUsuari;
+import static model.database.SQLoperations.borraUsuari;
 
 public class SettingViewController implements ActionListener {
 
     private SettingView sv;
+
 
     public SettingViewController(SettingView sv) {
         this.sv = sv;
@@ -22,19 +25,33 @@ public class SettingViewController implements ActionListener {
     @Override
     public void actionPerformed(ActionEvent e) {
         if (e.getActionCommand().equals("Logout")) { //cuando apretamos el boton
-            RegisterView rv = new RegisterView();
-            RegisterViewController rvc = new RegisterViewController(rv);
-            rv.mainController(rvc);
+            int confirmado = JOptionPane.showConfirmDialog(null,"Seguro que quieres hacer log out?");
+            if(JOptionPane.OK_OPTION == confirmado){
+                sv.setVisible(false);
+                LoginView lv = new LoginView();
+                LoginViewController lvc = new LoginViewController(lv);
+                lv.mainController(lvc);
+            }
+
         }
         if (e.getActionCommand().equals("Delete")) { //cuando apretamos el boton
-            RegisterView rv = new RegisterView();
-            RegisterViewController rvc = new RegisterViewController(rv);
-            rv.mainController(rvc);
+            int confirmado = JOptionPane.showConfirmDialog(null,"Seguro que quieres borrar la Cuenta?");
+            if(JOptionPane.OK_OPTION == confirmado){
+                sv.setVisible(false);
+                System.out.println("Arar Borrem conta");
+                //borraUsuari("pepe");//aqui va el nom del usuari de la persona
+                RegisterView rv = new RegisterView();
+                RegisterViewController rvc = new RegisterViewController(rv);
+                rv.mainController(rvc);
+
+            }
+
         }
         if (e.getActionCommand().equals("Config")) { //cuando apretamos el boton
             RegisterView rv = new RegisterView();
             RegisterViewController rvc = new RegisterViewController(rv);
             rv.mainController(rvc);
+            sv.setVisible(false);
         }
     }
 }
